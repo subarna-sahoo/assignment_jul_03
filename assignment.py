897456fd9608225448476384e3c8dad164ec106d
subarna-sahoo <er.subarnasahoo@gmail.com>
2021-07-03 18:20:25 +0530

    done.

---
+++ assignment.py
@@ -0,0 +1,35 @@
+def get_missing_range(lst, lower, upper):
+    temp_missing_range_lst = []
+    
+    if lst[0]-lower == 2:
+        temp_missing_range_lst.append(str(lower+1))
+    elif lst[0]-lower > 2:
+        temp_missing_range_lst.append(f"{lower+1}->{lst[0]-1}")
+
+    temp_list = lst
+    temp_list.append(upper)
+
+    for idx, num in enumerate(temp_list):
+        if idx < len(temp_list)-1:
+            start_num = num
+            next_num = temp_list[idx+1]
+            # print(start_num, next_num)
+            if next_num-start_num == 2:
+                temp_missing_range_lst.append(str(start_num+1))
+            elif next_num-start_num > 2:
+                temp_missing_range_lst.append(f"{start_num+1}->{next_num-1}")
+            else:
+                pass
+
+    return temp_missing_range_lst
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    lst = [0,1,3,50,75]
+
+    print(get_missing_range(lst, lower=0, upper=99))
